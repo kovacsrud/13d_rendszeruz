e319e1e95760f0b217e513b371989c7f350e43c8
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-22 10:11:31 +0200

    Python kezdés

---
+++ start.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 10:00:04 2020
+
+@author: SysAdmin
+"""
+
+print("Hello world!")
+
+# Ebben az esetben a számítás
+# eredménye jelenik meg
+print(2+2+6)
+
+# Ha kell zárójeleket használunk
+print(2+(6*10))
+
+# Az eredmény egy tört szám (float)
+print(10/3)
+
+# hibát okoz 
+print(10/0)
+
+
+
+
+
+
