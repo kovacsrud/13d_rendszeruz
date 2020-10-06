c9e2d9a2db3c72693d54e552614abc6457b04dcb
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-06 08:59:57 +0200

    Create honerseklet.py

---
+++ honerseklet.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  6 08:43:31 2020
+
+@author: SysAdmin
+"""
+
+
+# Írjon programot, amely hőmérséklet adatokat
+# kér be. A bekérés addig folytatódjon, amíg
+# negatív értéket nem adnak meg.
+
