4f6d1e684b8b4d8323fccd5834f820ee7df48797
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-13 13:54:21 +0200

    Update osszegzes.py

--- osszegzes.py
+++ osszegzes.py
@@ -29,4 +29,27 @@ for i in range(0,len(szamok)):
 print("Az összeg:"+str(osszeg))    
 print("Átlag:"+str(osszeg/len(szamok)))
 
+osszeg=0
+# valósítsuk meg az összegzést while ciklussal
+szamlalo=0
+while(szamlalo<len(szamok)):
+    osszeg+=szamok[szamlalo]
+    print(str(szamlalo)+","+str(szamok[szamlalo]))
+    szamlalo+=1
+    
+print("Az összeg:"+str(osszeg))    
+print("Átlag:"+str(osszeg/len(szamok)))    
+    
+#min, max kiválasztás
+min=szamok[0]
+max=szamok[0]
+
+for i in range(0,len(szamok)):
+    if(szamok[i]<min):
+        min=szamok[i]
+    if(szamok[i]>max):
+        max=szamok[i]
+        
+print("Min:"+str(min))
+print("Max:"+str(max))
 
