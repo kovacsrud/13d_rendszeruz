49d57222d27b7fc3ac5eb94f001b313dc288dd31
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-20 08:10:56 +0200

    Update color.py

--- color.py
+++ color.py
@@ -9,6 +9,12 @@ Created on Tue Oct 20 07:34:40 2020
 
 from colorama import Fore,Back,Style
 
-print(Fore.GREEN+"Zöld betűk")
+print(Fore.GREEN+"Zöld betűk"+Style.RESET_ALL)
+print(Fore.BLUE,Back.MAGENTA,Style.NORMAL+"Magenta háttér"+Style.RESET_ALL)
+print("Nincs szín elvileg")
 
+
+szamok=[19,233,45,68,122,411,62,69,76]
+
+#Jelöljük meg háttér és szövegszínnel a szamok lista 2-vel osztható számait.
 
