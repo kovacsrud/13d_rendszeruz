0da066fd1fd3990fe8272ddc525cfb1beab09295
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-11-03 08:26:36 +0100

    Update kivalogatas.py

--- kivalogatas.py
+++ kivalogatas.py
@@ -28,4 +28,26 @@ for i in range(0,len(szamok)):
         darab+=1
 
 print(darab)
+
+#Összegzés    
+osszeg=0
+
+for i in range(0,len(szamok)):
+    osszeg+=szamok[i]
     
+print(osszeg)    
+
+#Csak a pozitívak összege
+osszeg=0
+
+for i in range(0,len(szamok)):
+    if(szamok[i]>-1):
+        osszeg+=szamok[i]
+
+print(osszeg)        
+
+#Minimum, maximum kiválasztása
+min=szamok[0]
+max=szamok[0]
+
+
