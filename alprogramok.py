971d3170e2a6d526c42dee2e39b78e47d330d4f8
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-11-03 09:43:30 +0100

    Update alprogramok.py

--- alprogramok.py
+++ alprogramok.py
@@ -8,5 +8,23 @@ Created on Tue Nov  3 08:58:57 2020
 
 import random as rnd
 
+def veletlenlista(db):
+    lista=[]
+    for i in range(0,db):
+        lista.append(rnd.randint(0,100))
+    return lista
 
+##a veletlenlista függvény meghívása
+
+szamok=veletlenlista(10) 
+szamok2=veletlenlista(30)   
+
+print(szamok)
+print(szamok2)
+
+
+
+
+
+    
 
