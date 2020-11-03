23cc9c9b61d66e140678c9d81149e7e5aaab51f3
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-11-03 09:49:50 +0100

    Update alprogramok.py

--- alprogramok.py
+++ alprogramok.py
@@ -8,16 +8,17 @@ Created on Tue Nov  3 08:58:57 2020
 
 import random as rnd
 
-def veletlenlista(db):
+def veletlenlista(db,also,felso):
     lista=[]
     for i in range(0,db):
-        lista.append(rnd.randint(0,100))
+        lista.append(rnd.randint(also,felso))
     return lista
+
 
 ##a veletlenlista függvény meghívása
 
-szamok=veletlenlista(10) 
-szamok2=veletlenlista(30)   
+szamok=veletlenlista(10,0,100) 
+szamok2=veletlenlista(30,-100,100)   
 
 print(szamok)
 print(szamok2)
