2df8ce7b06df7eb8b305b8c5a7a40bf497d503be
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-11-10 13:21:09 +0100

    Update np.py

--- np.py
+++ np.py
@@ -8,7 +8,7 @@ Created on Tue Nov 10 11:25:21 2020
 import numpy as np
 import random as rnd
 
-szamok=[11,22,36,69,112,336,788,211,678,223]
+szamok=[11,22,36,69,112,336,788,211,678,223,626,6969]
 
 npszamok=np.array(szamok)
 
@@ -39,4 +39,17 @@ print(npszamok[npszamok>40].sum())
 #kiválogatások, több feltétel
 print((npszamok>40) & (npszamok<200))
 print(npszamok[(npszamok>40) & (npszamok<200)])
+
+# elemek kiválogatása index szerint
+print(npszamok)
+print(npszamok[3:6])
+print(npszamok[3:])
+print(npszamok[:3])
+
+print(npszamok.shape)
+print(npszamok.reshape(2,6))
+print(npszamok.reshape(6,2))
+print(npszamok.reshape(3,4))
+print(npszamok.reshape(4,3))
+
 
